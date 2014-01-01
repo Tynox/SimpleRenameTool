a9687dee1c504092c1b7c83754188c8b771c9638
Tynox <pocktynox@gmail.com>
2014-01-01 20:21:24 +0800

    init. not work.
    Init here. It's not a work version.

---
+++ rename_tool.py
@@ -0,0 +1,42 @@
+﻿#!/usr/bin/env python
+# encoding:utf-8
+
+"""
+批量重命名文件小工具
+version: 0.1
+"""
+
+import os
+import sys
+import getopt
+
+# get argv
+try:
+    #dir = sys.argv[1]
+    opts, args = getopt.getopt(argv[1:], "d:n", ["dir=", "name"]
+    print "dir:{0}".format(dir)
+except getopt.GetoptError, err:
+    print str(err)
+    sys.exit()
+
+# parse argv
+try:
+    # fileList = os.listdir(dir)
+    for o, a in opts:
+        if o in ("-d", "--dir"):
+            fileList = os.listdir(a)
+        elif o in ("-n", "--name"):
+            pass
+except:
+    print "Error! Failed to find the folder."
+    
+#elif isinstance(fileList, str):
+#    print "This is an undecodable filename."
+else:
+    try:
+        for i, filename in enumerate(fileList):
+            os.renames(dir+filename, "{0}{1}.png".format(dir, i+1))
+    except:
+        print "Error! Failed to rename files. Check the existing filenames."
+    else:
+        print "Done!"
